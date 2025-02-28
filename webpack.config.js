e3c11af663700e0a71fca0870a45cf817eabaaf2
Aljon Lerios <aljonclerios@gmail.com>
2025-02-28 11:59:37 +0800

    chore: Update dependencies and add webpack configuration for extension build

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+//@ts-check
+
+"use strict";
+
+const path = require("path");
+
+/**@type {import('webpack').Configuration}*/
+const config = {
+  target: "node", // vscode extensions run in a Node.js-context
+  entry: "./src/extension.ts", // the entry point of this extension
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "extension.js",
+    libraryTarget: "commonjs2",
+    devtoolModuleFilenameTemplate: "../[resource-path]",
+  },
+  externals: {
+    vscode: "commonjs vscode", // the vscode-module is created on-the-fly and must be excluded
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "ts-loader",
+          },
+        ],
+      },
+    ],
+  },
+};
+
+module.exports = config;
